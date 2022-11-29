90fb2cd37d83af09bfc42244a9dbd5d4f1d349c5
Asiyacm <asiyacmuhammed@gmail.com>
2022-11-29 12:20:01 +0530

    api commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { SearchFriendComponent } from './search-friend/search-friend.component';
 import { RouterModule, Routes } from '@angular/router';
 import { NavigationbarComponent } from './navigationbar/navigationbar.component';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
 
 
 const myRoute:Routes=[
@@ -39,7 +40,8 @@ const myRoute:Routes=[
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot(myRoute),
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
